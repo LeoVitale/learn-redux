1376754552a6e2bc8493e54beef65e5ca5247f99
Leonardo Vitale <leonardo_vitale@hotmail.com>
2016-05-18 18:15:45 -0300

    Creating our Single and PhotoGrid components

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render(){
+    <div className="photo-grid">
+      PHOTO GRID
+    </div>
+  }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render(){
+    <div className="single-photo">
+      SINGLE PHOTO
+    </div>
+  }
+});
+
+export default Single;
